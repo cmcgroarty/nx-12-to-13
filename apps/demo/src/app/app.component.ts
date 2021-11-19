b1e0fa4653874b457e78be02b494f3bd1c0686d1
Chris McGroarty <iammarix@gmail.com>
2021-11-19 13:01:00 -0500

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx12to13-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
